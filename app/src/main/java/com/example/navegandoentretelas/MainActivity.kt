0ca8bc35fe0364a41d76ddae08d80c484f6777bb
Enzo-Grisolia <enzogrisolia.souza@gmail.com>
2026-03-17 20:03:31 -0300

    Implementa NavController na MainActivity para gerenciar navegação entre telas com Navigation Compose

--- app/src/main/java/com/example/navegandoentretelas/MainActivity.kt
+++ app/src/main/java/com/example/navegandoentretelas/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.navegandoentretelas
 
 import android.os.Bundle
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -9,6 +12,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
 import com.example.navegandoentretelas.screens.LoginScreen
+import com.example.navegandoentretelas.screens.MenuScreen
+import com.example.navegandoentretelas.screens.PedidosScreen
+import com.example.navegandoentretelas.screens.PerfilScreen
 import com.example.navegandoentretelas.ui.theme.NavegandoEntreTelasTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,7 +24,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             NavegandoEntreTelasTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    LoginScreen(modifier = Modifier.padding(innerPadding))
+                    val navController = rememberNavController()
+
+                    NavHost(
+                        navController = navController,
+                        startDestination = "login",
+                    ) {
+                        composable(route = "login") {
+                            LoginScreen(modifier = Modifier.padding(innerPadding))
+                        }
+                        composable(route = "menu") {
+                            MenuScreen(modifier = Modifier.padding(innerPadding))
+                        }
+                        composable(route = "pedidos") {
+                            PedidosScreen(modifier = Modifier.padding(innerPadding))
+                        }
+                        composable(route = "perfil") {
+                            PerfilScreen(modifier = Modifier.padding(innerPadding))
+                        }
+                    }
 
                 }
             }
