fa78c35fcbc076fb2748b07c31b41d7aa6edd723
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-24 19:57:18 -0300

    Subindo novo commit para CheckPoint

--- app/src/main/java/com/example/navegandoentretelas/MainActivity.kt
+++ app/src/main/java/com/example/navegandoentretelas/MainActivity.kt
@@ -11,6 +11,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavType
+import androidx.navigation.navArgument
 import com.example.navegandoentretelas.screens.LoginScreen
 import com.example.navegandoentretelas.screens.MenuScreen
 import com.example.navegandoentretelas.screens.PedidosScreen
@@ -22,7 +24,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            NavegandoEntreTelasTheme {
+            NavegandoEntreTelasTheme {  }
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     val navController = rememberNavController()
 
@@ -36,16 +38,36 @@ class MainActivity : ComponentActivity() {
                         composable(route = "menu") {
                             MenuScreen(modifier = Modifier.padding(innerPadding), navController)
                         }
-                        composable(route = "pedidos") {
-                            PedidosScreen(modifier = Modifier.padding(innerPadding), navController)
+                        composable(
+                            route = "pedidos?cliente={cliente}",
+                            arguments = listOf(navArgument("cliente") {
+                                defaultValue = "Cliente Genérico"
+                            })
+                        ) {
+                            PedidosScreen(
+                                modifier = Modifier.padding(innerPadding),
+                                navController,
+                                it.arguments?.getString("cliente")
+                            )
                         }
-                        composable(route = "perfil") {
-                            PerfilScreen(modifier = Modifier.padding(innerPadding), navController)
+                            composable(
+                                route = "perfil/{nome}/{idade}",
+                                arguments = listOf(
+                                    navArgument("nome") { type = NavType.StringType },
+                                    navArgument("idade") { type = NavType.IntType }
+                                )
+                            ) {
+                                val nome: String? = it.arguments?.getString("nome", "Usuário Genérico")
+                                val idade: Int? = it.arguments?.getInt("idade", 0)
+                                PerfilScreen(
+                                    modifier = Modifier.padding(innerPadding),
+                                    navController,
+                                    nome!!,
+                                    idade!!
+                                )
+                            }
                         }
                     }
-
                 }
             }
         }
-    }
-}
